585c87f50257a82e34f10e521edee5b74798226c
irma213 <irma33037@gmail.com>
2025-05-30 20:27:15 +0800

    Create create-document.js
    Menambahkan perintah insertOne() untuk membuat dokumen nilai mahasiswa baru di collection 'nilai_mahasiswa'.

---
+++ create-document.js
@@ -0,0 +1,13 @@
+// create-document.js
+
+// Gunakan di Mongo Shell
+
+use kampus; // Pastikan berada di database "kampus"
+
+db.nilai_mahasiswa.insertOne({
+  nim: "D0222007",
+  nama: "Irmayanti",
+  matkul: "Basis Data",
+  nilai: 85,
+  semester: 4
+})
